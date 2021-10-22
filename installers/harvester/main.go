8296af2c4a532c8c13a0cc2b0ccb0c51c0825e07
Gaurav Mehta <gaurav@rancher.com>
2021-10-22 15:44:31 +1100

    changed order of commands

--- installers/harvester/main.go
+++ installers/harvester/main.go
@@ -28,17 +28,13 @@ func bootScriptHarvester(ctx context.Context, j job.Job, s ipxe.Script, version 
 	} else {
 		s.Set("base-url", "https://releases.rancher.com/harvester")
 	}
-
-	if version == "v0.2.0" {
-		s.Kernel(fmt.Sprintf("${base-url}/%s/harvester-vmlinuz-amd64", version))
-		s.Initrd(fmt.Sprintf("${base-url}/%s/harvester-initrd-amd64", version))
-	} else {
-		s.Kernel(fmt.Sprintf("${base-url}/%s/harvester-%s-vmlinuz-amd64", version, version))
-		s.Initrd(fmt.Sprintf("${base-url}/%s/harvester-%s-initrd-amd64", version, version))
-	}
-
 	j.With("parsed userdata", j.UserData())
 	ks := kernelParams(j, s, version)
+	if version == "v0.2.0" {
+		ks.Initrd(fmt.Sprintf("${base-url}/%s/harvester-initrd-amd64", version))
+	} else {
+		ks.Initrd(fmt.Sprintf("${base-url}/%s/harvester-%s-initrd-amd64", version, version))
+	}
 	ks.Boot()
 	// once boot script is served no long want this, since harvester install triggers a
 	// reboot of the node
@@ -54,10 +50,11 @@ func kernelParams(j job.Job, s ipxe.Script, version string) ipxe.Script {
 
 	switch version {
 	case "v0.2.0":
+		s.Kernel(fmt.Sprintf("${base-url}/%s/harvester-vmlinuz-amd64", version))
 		s.Args("k3os.mode=install", "k3os.debug", "console=tty1,115200", "harvester.install.automatic=true", "boot_cmd=\"echo include_ping_test=yes >> /etc/conf.d/net-online\"")
 	case "v0.3.0":
-		s.Args("rd.cos.disable", "rd.noverifyssl", "net.ifnames=1,console=tty1", "harvester.install.automatic=true")
-		s.Args(fmt.Sprintf("root=live:https://releases.rancher.com/harvester/%s/harvester-%s-rootfs-amd64.squashfs", version, version))
+		s.Kernel(fmt.Sprintf("${base-url}/%s/harvester-%s-vmlinuz-amd64", version, version))
+		s.Args("rd.cos.disable", "rd.noverifyssl", "net.ifnames=1", "console=tty1")
 	}
 	if len(currentUserData) != 0 {
 		s.Args(currentUserData)
