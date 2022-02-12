52be3dcf1bf90ca476f21b57463f4378a4c769ae
Gaurav Mehta <gaurav@rancher.com>
2022-02-13 09:41:14 +1100

    tweaks for base url override

--- installers/ros2/main.go
+++ installers/ros2/main.go
@@ -2,6 +2,7 @@ package ros2
 
 import (
 	"context"
+
 	"github.com/tinkerbell/boots/ipxe"
 	"github.com/tinkerbell/boots/job"
 )
@@ -13,17 +14,19 @@ func (i Installer) BootScript() job.BootScript {
 		s.PhoneHome("provisioning.104.01")
 		s.Set("arch", j.Arch())
 		s.Set("version", j.OperatingSystem().Version)
-		s.Set("url", "https://github.com/rancher-sandbox/os2/releases/download/${version}")
-		s.Set("kernel", "rancheros-${version}-${arch}-kernel")
-		s.Set("initrd", "rancheros-${version}-${arch}-initrd")
+		if j.OSIEBaseURL() != "" {
+			s.Set("url", j.OSIEBaseURL())
+		} else {
+			s.Set("url", "https://github.com/rancher-sandbox/os2/releases/download/${version}")
+		}
 		s.Set("rootfs", "rancheros-${version}-${arch}.squashfs")
 		s.Set("iso", "rancheros-${version}-${arch}.iso")
-		s.Kernel("${base-url}/vmlinuz")
-		s.Args("${url}/${kernel}", "initrd=${initrd}", "ip=dhcp", "rd.cos.disable", "root=live:${url}/${rootfs}",
+		s.Kernel("${url}/rancheros-${version}-${arch}-kernel")
+		s.Args("ip=dhcp", "rd.cos.disable", "root=live:${url}/${rootfs}",
 			"rancheros.install.automatic=true", "rancheros.install.iso_url=${url}/${iso}",
-			"rancheros.install.config_url=${config}", "console=tty1")
+			"console=tty1")
 		s.Args(j.UserData())
-		s.Initrd("rancheros-${version}-${arch}-initrd")
+		s.Initrd("${url}/rancheros-${version}-${arch}-initrd")
 		s.Boot()
 		j.DisablePXE(ctx)
 		return s
